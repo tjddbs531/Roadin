1958d671745e6b20c7949ee0f63d7cbb85fc8b31
mingyeong0210 <mica7150@naver.com>
2025-03-11 02:24:14 +0900

    feat: 선호 해시태그 UI

--- Frontend/src/pages/Main/MyPage/MyPage.js
+++ Frontend/src/pages/Main/MyPage/MyPage.js
@@ -1,10 +1,36 @@
 import React, {useState} from 'react';
+import { useNavigate } from "react-router-dom";
 import './MyPage.css';
 import profile from '../../../assets/img/profile.svg';
-import { useNavigate } from "react-router-dom";
+import x from '../../../assets/img/x.svg';
+import plus from '../../../assets/img/plus.svg';
 
 function MyPage() {
   const navigate = useNavigate();
+
+  const [viewAllTags, setViewAllTags] = useState(false);
+  const [activeTag, setActiveTag] = useState([]);
+
+  const viewAll = () => { // 전체 태그 확인하는 함수
+    setViewAllTags(!viewAllTags);
+  }
+
+  // 태그 클릭 시 active 클래스를 토글하는 함수
+  const toggleTag = (tag) => {
+    setActiveTag((prevActiveTag) => {
+      if (prevActiveTag.includes(tag)) {
+        // 이미 활성화된 태그라면 비활성화
+        return prevActiveTag.filter((t) => t !== tag);
+      } else {
+        // 비활성화된 태그라면 활성화
+        return [...prevActiveTag, tag];
+      }
+    });
+  }
+
+  const removeTag = (tag) => {
+    setActiveTag((prevActiveTag) => prevActiveTag.filter((t) => t !== tag));
+  };
   
   return (
     <div className='mypage_container'>
@@ -29,6 +55,37 @@ function MyPage() {
                   <button className='delete_btn'>계정탈퇴</button>
                   <button className='edit_btn' onClick={() => navigate("/myPage/edit")}>수정하기</button>
                 </div>
+            </div>
+
+            <div className='like_tags'>
+              <p className='like_title'>선호 해시태그</p>
+              
+              <div className='tags_container'>
+                <div className='tag' style={{color: 'black'}}>#</div>
+                {activeTag.map((tag, index) => (
+                   <div key={index} className='tag'>
+                   {tag}
+                   <img
+                     src={x} // X 아이콘
+                     alt='remove'
+                     onClick={() => removeTag(tag)} // 클릭 시 해당 태그 비활성화
+                     style={{ cursor: 'pointer', marginLeft: '8px' }}
+                   />
+                 </div>
+                ))}
+                <div className='tag' style={{color: 'black', cursor: 'pointer'}} onClick={() => viewAll()}>
+                  <img src={plus} width={14}/>
+                </div>
+              </div>
+
+              {viewAllTags && (
+                  <div className='all_tags_container'>
+                    <div className={`tags ${activeTag.includes('힐링') ? 'active' : ''}`} onClick={() => toggleTag('힐링')}>힐링</div>
+                    <div className={`tags ${activeTag.includes('활동적인') ? 'active' : ''}`} onClick={() => toggleTag('활동적인')}>활동적인</div>
+                    <div className={`tags ${activeTag.includes('친구들과 함께하는') ? 'active' : ''}`} onClick={() => toggleTag('친구들과 함께하는')}>친구들과 함께하는</div>
+                    <div className={`tags ${activeTag.includes('부모님과 함께하는') ? 'active' : ''}`} onClick={() => toggleTag('부모님과 함께하는')}>부모님과 함께하는</div>
+                  </div>
+              )}
             </div>
         </div>
     </div>
