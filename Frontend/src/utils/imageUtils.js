12bdd23bff27e328b7acc5dcef9a0cdf218a852c
tory99 <hjw990826@gmail.com>
2025-03-20 18:46:59 +0900

    Signup button fix

--- Frontend/src/utils/imageUtils.js
+++ Frontend/src/utils/imageUtils.js
@@ -2,22 +2,22 @@ export const getImageForPlace = (placeName) => {
     switch (placeName) {
       case '서울':
         return 'https://cdn.pixabay.com/photo/2022/09/16/17/07/city-7459162_1280.jpg';
-      case '경기도':
-        return 'https://cdn.pixabay.com/photo/2017/10/09/02/45/korea-2832271_1280.jpg';
-      case '강원도':
-        return 'https://cdn.pixabay.com/photo/2020/03/09/08/53/seorak-4915009_1280.jpg';
-      case '충청북도':
-        return 'https://cdn.pixabay.com/photo/2022/02/13/05/48/dodamsambong-peaks-7010408_1280.jpg';
-      case '충청남도':
-        return 'https://cdn.pixabay.com/photo/2016/08/17/20/05/korea-1601370_1280.jpg';
-      case '경상북도':
-        return 'https://cdn.pixabay.com/photo/2021/01/14/00/30/autumn-5915468_1280.jpg';
-      case '전라북도':
-        return 'https://cdn.pixabay.com/photo/2018/08/23/22/18/jeonju-3626873_1280.jpg';
-      case '경상남도':
-        return 'https://cdn.pixabay.com/photo/2020/05/24/11/58/to-5213935_1280.jpg';
-      case '전라남도':
-        return '';
+      case '대전':
+        return 'https://i.namu.wiki/i/8FyieVzq1ufJT_AXLuFqQYYhke0_k64dTMiXI-RTNai2PHpGNKvLgejhH_XFuCfUJUtaJQ52WfZvzbfMKxDrxmjukgtUeeOdVWJc8ttnKwwBvKZFFo0VfhYF1sXmV9UxWitQfqDMp3zKUUGtgm5DIQ.webp';
+      case '부산':
+        return 'https://i.namu.wiki/i/2C6UPE7J66hX_e0f0xqKutGZu_LHKHLRfMqNGJbFM-8udxFmnGfF17bSXH8yBl9vF3f8ctje-T-qMQlikRdd46U-ZPP-t2m-oDV7X_igv1QEZg3bzJ2cJ0E9u6rnHK50tbT8b-lfPBCP65UVZ2DRRQ.webp';
+      case '인천':
+        return 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Incheon_Station_20230430_013.jpg/300px-Incheon_Station_20230430_013.jpg';
+      case '도쿄':
+        return 'https://i.namu.wiki/i/ultfBOlLmDn7xGH8qpy2gGUkRA6kXuzqwqFHUSPnxk61suj0LFMymgrss9EnCtzEeywrhGJhZTn09iPx5FkW8IedMBcX71P4z1snHvW6i-0deXFZY8liOBji2EjRuS_3e8l3wSJ0aIlQUqwAgUPhKw.webp';
+      case '후쿠오카':
+        return 'https://thumb.tidesquare.com/common_cdn/upload/image/place/tmp/12273_2.jpg';
+      case 'LA':
+        return 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/LA_Skyline_Mountains2.jpg/1024px-LA_Skyline_Mountains2.jpg';
+      case '로마':
+        return 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Colosseum_in_Rome%2C_Italy_-_April_2007.jpg/1200px-Colosseum_in_Rome%2C_Italy_-_April_2007.jpg';
+      case '파리':
+        return 'https://asset-prod.france.fr/Eiffel_Tower_at_sunset_in_Paris_France_Romantic_travel_background_Man79_Adobe_Stock_8aa81830ce.jpeg';
       case '제주도':
         return 'https://cdn.pixabay.com/photo/2022/04/28/19/47/republic-of-korea-7161860_1280.jpg';
       default:
