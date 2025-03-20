03ce3be6c3dea14612b1cc9ac878d5decd6a41c8
jieun <jieunh0311@gmail.com>
2025-03-20 15:39:54 +0900

    검색 기능 이미지 추가

--- Frontend/src/components/PlaceList/PlaceList.js
+++ Frontend/src/components/PlaceList/PlaceList.js
@@ -1,27 +1,36 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import './PlaceList.css';
+import { getImageForPlace } from "../../utils/imageUtils";
+import "./PlaceList.css";
 
-function PlaceList({placesData}) {
-    const navigate = useNavigate();
-  
+function PlaceList({ placesData }) {
+  const navigate = useNavigate();
+
   return (
     <div>
-    { placesData.map((place, index) => (
-        <div className="list_container" onClick={() => navigate(`/place/${place.place_name}`)}>
-            <div className="place_img_box"></div>
-            <div className="list_text_container">
-                <div className="list_content_container">
-                    <p className="list_title">{place.place_name}</p>
-                    <p className="list_intro">{place.place_info}</p>
-                </div>
-                
+      {placesData.map((place, index) => (
+        <div
+          className="list_container"
+          onClick={() => navigate(`/place/${place.place_name}`)}
+        >
+          <div
+            className="place_img_box"
+            style={{
+              backgroundImage: `url(${getImageForPlace(place.place_name)})`,
+            }}
+          ></div>
+
+          <div className="list_text_container">
+            <div className="list_content_container">
+              <p className="list_title">{place.place_name}</p>
+              <p className="list_intro">{place.place_info}</p>
             </div>
+          </div>
         </div>
-    ))}
+      ))}
     </div>
-  )
+  );
 }
 
 export default PlaceList;
